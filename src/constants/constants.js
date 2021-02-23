b802fff7c11e7e054c8b92244f4865029398b4fd
kennedy muthaura <kennedymuthaura99@gmail.com>
2021-02-23 16:11:58 +0300

    changed urls to heruko

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,11 @@ export const DASHBOARD = "/dashboard";
 export const SETTINGS = "/dashboard/profile";
 // **************** ROUTE CONSTANT END **************************
 
-export const BASE_URL = process.env.API_URL || "https://techminia.com";
+// export const BASE_URL = process.env.API_URL || "https://techminia.com";
+export const BASE_URL =
+  process.env.API_URL || "http://darasa-mvp.herokuapp.com";
 export const BASE_WEBSOCKET_URL =
-  process.env.API_WEBSOCKET_URL || "wss://techminia.com";
+  process.env.API_WEBSOCKET_URL || "ws://127.0.0.1:8000";
+// export const BASE_WEBSOCKET_URL =
+//   process.env.API_WEBSOCKET_URL || "wss://techminia.com";
 export const CURRENCY = "Ksh.";
