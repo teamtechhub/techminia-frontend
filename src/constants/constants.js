c756ca1e4ae8c94c6ff28607eb49bda47da9c28a
kennedy muthaura <kennedymuthaura99@gmail.com>
2021-02-24 11:24:15 +0300

    production backend with server

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,12 +4,12 @@ export const DASHBOARD = "/dashboard";
 export const SETTINGS = "/dashboard/profile";
 // **************** ROUTE CONSTANT END **************************
 
-// export const BASE_URL = process.env.API_URL || "https://techminia.com";
-export const BASE_URL =
-  process.env.API_URL || "https://darasa-mvp.herokuapp.com";
-export const BASE_WEBSOCKET_URL =
-  process.env.API_WEBSOCKET_URL || "wss://darasa-mvp.herokuapp.com";
-
+export const BASE_URL = process.env.API_URL || "https://techminia.com";
+// export const BASE_URL =
+//   process.env.API_URL || "https://darasa-mvp.herokuapp.com";
 // export const BASE_WEBSOCKET_URL =
-//   process.env.API_WEBSOCKET_URL || "wss://techminia.com";
+//   process.env.API_WEBSOCKET_URL || "wss://darasa-mvp.herokuapp.com";
+
+export const BASE_WEBSOCKET_URL =
+  process.env.API_WEBSOCKET_URL || "wss://techminia.com";
 export const CURRENCY = "Ksh.";
