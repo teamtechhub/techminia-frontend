bd28d88ca6e86c57f2af475093c000274e6c4ccf
kennedy muthaura <kennedymuthaura99@gmail.com>
2021-03-09 10:57:48 +0300

    checking the build for frontend

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -9,14 +9,14 @@ export const SETTINGS = "/dashboard/profile";
 // export const BASE_WEBSOCKET_URL =
 //   process.env.API_WEBSOCKET_URL || "wss://darasa-mvp.herokuapp.com";
 
-export const BASE_URL = process.env.API_URL || "https://techminia.com";
-export const BASE_WEBSOCKET_URL =
-  process.env.API_WEBSOCKET_URL || "wss://techminia.com";
+// export const BASE_URL = process.env.API_URL || "https://techminia.com";
+// export const BASE_WEBSOCKET_URL =
+//   process.env.API_WEBSOCKET_URL || "wss://techminia.com";
 export const CURRENCY = "Ksh.";
 
 //for local developement
 
-// export const BASE_WEBSOCKET_URL =
-//   process.env.API_WEBSOCKET_URL || "ws://localhost:8000";
+export const BASE_WEBSOCKET_URL =
+  process.env.API_WEBSOCKET_URL || "ws://localhost:8000";
 
-// export const BASE_URL = process.env.API_URL || "http://localhost:8000";
+export const BASE_URL = process.env.API_URL || "http://localhost:8000";
